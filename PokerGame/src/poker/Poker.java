c0958d7641533dadce4ee637dd9d79a7afab548b
jwkeung <jwkeung@jkeung1.cs.cityu.edu.hk>
2022-09-15 15:57:39 +0800

    added spaces

--- PokerGame/src/poker/Poker.java
+++ PokerGame/src/poker/Poker.java
@@ -35,6 +35,9 @@ public class Poker {
 	public static void main(String args[]) {
 		System.out.println(new Poker().isFullHouse(
 			new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+		
+		
+		
 	}
 }
 // end of Poker.java
